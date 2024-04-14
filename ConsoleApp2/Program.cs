ac59d823c8a033b3d4bbdd4492e074d29ee87e15
KFH <KFH@RAZAN-ALSHATTI>
2024-04-14 15:33:24 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//task Functions
+//challenge 1
+Console.WriteLine("Task Functions: \n challenge 1:");
+
+void GreetPerosn(string name)
+{
+    Console.WriteLine($"hello {name}");
+}
+var name = "Razan";
+GreetPerosn(name);
+
+public class Greater()
+{
+    public void GreetPerson(string name)
+    {
+        Console.WriteLine($"Hrllo {name}");
+    }
+    public void GreetPerson()
+    {
+        GreetPerson("Unkown");
+    }
+}
+
+
